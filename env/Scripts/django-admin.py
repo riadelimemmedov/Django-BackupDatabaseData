f5c48f5183d9171303094540c34b9e7a92590d39
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-02-27 07:56:18 +0400

    djangobackup

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\riade\OneDrive\Desktop\backupprojectdjango\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
